3c07d1c43ec97a184c99029e8068e18aedcf843e
Agam Raj <agamraj0411@gmail.com>
2022-01-14 21:49:25 +0530

    Length of Linked List

---
+++ lengthofLL.py
@@ -0,0 +1,42 @@
+
+
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+def takeInput():
+    inputList = [int(ele) for ele in input().split()]
+    head = None
+    for currentdata in inputList:
+        if currentdata == -1:
+            break
+        newNode = Node(currentdata)
+        if head is None:
+            head = newNode
+        else:
+            curr = head
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = newNode
+    return head
+
+def printll(head):
+    while head is not None:
+        print(str(head.data) ,"->", end = " ")
+        head = head.next
+    print("None")
+    return
+
+def lengthLL(head):
+    i = 0
+    while head is not None:
+        i=i+1
+        head = head.next
+    return i
+
+
+
+
+
